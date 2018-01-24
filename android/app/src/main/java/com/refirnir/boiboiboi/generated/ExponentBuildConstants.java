2b3e2243c822c40102e307b9e8186a7cc54b3f9c
agirmar92 <aegir13@ru.is>
2018-01-24 13:02:07 +0000

    App ejected

---
+++ android/app/src/main/java/com/refirnir/boiboiboi/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.refirnir.boiboiboi.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp66b1a431939742888ceb7d9f54f2b7e3://localhost:19000";
+
+}
---
+++ ios/Pods/Target Support Files/Pods-boi-boi-boi/Pods-boi-boi-boi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_boi_boi_boi : NSObject
+@end
+@implementation PodsDummy_Pods_boi_boi_boi
+@end
